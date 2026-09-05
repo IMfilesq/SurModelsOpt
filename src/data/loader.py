46d088346ea21d118e94c6da04e81476d5d8b64e
TreeCapitator <igor.misterowicz@gmail.com>
2026-09-05 11:59:58 +0200

    schemat analizy

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 
 
-processed = loader("data/raw.csv")
-analysis = reporter(processed, cfg.boundaries)
+processed = loader(raw.csv)
+analysis = reporter(processed, cnfg.boundaries)
 plot = analysis.plot
 stats = analysis.stats
-itp
-=======
-<<<<<<< HEAD
-print("kot")
-=======
-print("pies")
->>>>>>> 05026a8e8098e0b995e466620dbf673162bb1387
->>>>>>> 087ec1cce2c494f519c356360c3f2d0df51f05c6
