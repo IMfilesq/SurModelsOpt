e329a0dc4f62090ad0a1b7354ba9aa635ad6c915
TreeCapitator <igor.misterowicz@gmail.com>
2026-09-05 10:28:34 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import hydra
+import logging
+import pandas as pd
+from omegaconf import DictConfig
+
+logger = logging.getLogger(__name__)
+@hydra.main(config_path="config",
+            config_name="config",
+            version_base=None)
+
+def main(cfg: DictConfig) -> None:
+    logger.info("Reading data from csv file")
+    raw = pd.read_csv(cfg.data.data_path)
+
+
+if __name__ == "__main__":
+    main()
